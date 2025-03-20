993c16c196bea534a981bbc46b41517bac9520b8
KaifAliSayyad <kaifali.sayyad@wissen.com>
2025-03-20 14:53:55 +0530

    register_service

---
+++ register/demo/src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,37 @@
+package com.example.demo.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private LocalDate dob;
+
+    private Double balance;
+}
